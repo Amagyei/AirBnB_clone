a6d474602a1e26cda124d993a3acd7bb5464a756
amagyei <kwame.na@hotmail.com>
2024-03-10 14:39:53 -0700

    task

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 """ define a class Square """
+from datetime import datetime
 import uuid
 
-class Square:
+class BaseModel:
 	"""represents a base class"""
 
-	def __init__(self, id, created_at, updated_at):
-		""" initialise the base class """
+        def __init__(self, id: str, created_at: datetime, updated_at: datetime):
+		""" initialise the base class 
 
-		self.id = uuid.uuid1()
+                Args:
+                    id (str): The unique identifier for the square.
+                    created_at (datetime): The timestamp indicating when the square was created.
+                    updated_at (datetime): The timestamp indicating when the square was last updated.
+                """
+
+		self.id = id
 		self.created_at = created_at
 		self.updated_at = updated_at
 	
 	def __str__(self, id)
+                """Returns a string representation of the square."""
 		return f'[self.name] (self.id) self.__dict__'

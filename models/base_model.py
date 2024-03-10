2dbf06a307daf3e877ec9b9cc1c691c10e696570
amagyei <kwame.na@hotmail.com>
2024-03-10 20:37:32 +0000

    task

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" define a class Square """
+class Square:
+	"""represents a base class""""
+
+	def __init__(self, id, created_at, updated_at):
+		""" initialise the base class """
+
+		self.id = uuid.uuid1()
+		self.created_at = created_at
+		self.updated_at = updated_at
+	
+	def __str__(self, id)
+		return f'[self.name] (self.id) self.__dict__'

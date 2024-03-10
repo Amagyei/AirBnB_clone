9a349247f71ea75b80a9a48cd59e44ac1a24cc83
amagyei <kwame.na@hotmail.com>
2024-03-10 15:29:42 -0700

    task

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,50 @@
 #!/usr/bin/python3
-""" define a class Square """
+
+"""
+This file defines  the BaseModel class which will
+serve as the base of ou model.
+"""
+from uuid import uuid4
 from datetime import datetime
-import uuid
+import models
+
 
 class BaseModel:
-	"""represents a base class"""
+    """Base class for all our classes"""
 
-        def __init__(self, id: str, created_at: datetime, updated_at: datetime):
-		""" initialise the base class 
+    def __init__(self, *args, **kwargs):
+        """ deserialize and serialize a class """
 
-                Args:
-                    id (str): The unique identifier for the square.
-                    created_at (datetime): The timestamp indicating when the square was created.
-                    updated_at (datetime): The timestamp indicating when the square was last updated.
-                """
+        """initialize  if nothing is passed"""
+        if kwargs == {}:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
+            return
 
-		self.id = id
-		self.created_at = created_at
-		self.updated_at = updated_at
-	
-	def __str__(self, id)
-                """Returns a string representation of the square."""
-		return f'[self.name] (self.id) self.__dict__'
+        """using Key words (deserialize)"""
+        if 'id' not in kwargs:
+            kwargs['id'] = str(uuid4())
+        self.id = kwargs['id']
+
+        for Key, val in kwargs.items():
+            if Key == "__class_":
+                continue
+        if "created_at" in kwargs:
+            self.created_at = datetime.strptime(
+                    kwargs['created_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f')
+        if "updated_at" in kwargs:
+            self.updated_at = datetime.strptime(
+                    kwargs['updated_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f')
+
+    def __str__(self):
+        """overide str representation of self"""
+        fmt = "[{}] ({}) {}"
+        return fmt.format(
+                type(self).__name__,
+                self.id,
+                self.__dict__)
+
